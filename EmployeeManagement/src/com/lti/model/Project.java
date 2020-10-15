c75e5c8d6f569435dba67613e1bd15f64a5d8dba
Sarit Shekhar Gaine <saritgaine1997@gmail.com>
2020-10-15 13:12:28 +0530

    added project class

---
+++ EmployeeManagement/src/com/lti/model/Project.java
@@ -0,0 +1,18 @@
+package com.lti.model;
+
+public class Project {
+   int ProjectCode;
+   String ProjectName;
+public int getProjectCode() {
+	return ProjectCode;
+}
+public void setProjectCode(int projectCode) {
+	ProjectCode = projectCode;
+}
+public String getProjectName() {
+	return ProjectName;
+}
+public void setProjectName(String projectName) {
+	ProjectName = projectName;
+}
+}
